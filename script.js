a5e42236ff2d627c855f8839a72ceccb3a263caf
Liyajoseph <84517633+Liyajoseph@users.noreply.github.com>
2021-06-21 13:07:04 +0000

    <!DOCTYPE html> <html>   <head>     <meta charset="utf-8">     <meta name="viewport" content="width=device-width">     <title>24hr Digital Clock</title>     <link href="style.css" rel="stylesheet" type="text/css" />   </head>   <body>     <div id="link">       <h1><a href="https://editor.p5js.org/Liyajoseph/full/1LOD-LqQA">Drawing Canvas!</a></h1>     </div>     <div id="clock">         <div id="hour">00</div>         <span>:</span>         <div id="minute">00</div>         <span>:</span>         <div id="second">00</div>     </div>     <script src="script.js"></script>   </body> </html>

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // These are the let statements for the program which will let the computer know what stands for what.
-const hour = document.getElementById('hour'); ;
-const minute = document.getElementById('minute'); ;
-const second = document.getElementById('second'); ;
+const hour = document.getElementById('hour')
+const minute = document.getElementById('minute')
+const second = document.getElementById('second')
 
 // This is where the clock functions.
-const clock = setInterval(time, 1000); ;
+const clock = setInterval(time, 1000)
 function time () {
-  const dateNow = new Date(); ;
-  let hr = dateNow.getHours(); ;
-  let min = dateNow.getMinutes(); ;
-  let sec = dateNow.getSeconds(); ;
+  const dateNow = new Date()
+  let hr = dateNow.getHours()
+  let min = dateNow.getMinutes()
+  let sec = dateNow.getSeconds()
 
   // The if statements lets the computer know what to do in. certain situations.
   if (hr < 10) {
@@ -22,8 +22,8 @@ function time () {
     sec = '0' + sec
   }
 
-  hour.textContent = hr; ;
-  minute.textContent = min; ;
-  second.textContent = sec; ;
-  clock.textContent = clock; ;
+  hour.textContent = hr
+  minute.textContent = min
+  second.textContent = sec
+  clock.textContent = clock
 }
