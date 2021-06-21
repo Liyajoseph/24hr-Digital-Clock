7af3b548cea72c8e2faeafb3021c29f666fa2899
Liyajoseph <84517633+Liyajoseph@users.noreply.github.com>
2021-06-21 03:19:18 +0000

    <!DOCTYPE html> <html>   <head>     <meta charset="utf-8">     <meta name="viewport" content="width=device-width">     <title>24hr Digital Clock</title>     <link href="style.css" rel="stylesheet" type="text/css" />   </head>   <body>     <div id="link">     <h1><a href="https://editor.p5js.org/Liyajoseph/full/1LOD-LqQA">Drawing Canvas!</a></h1>     </div>     <div id="clock">         <div id="hour">00</div>         <span>:</span>         <div id="minute">00</div>         <span>:</span>         <div id="second">00</div>     </div>     <script src="script.js"></script>   </body> </html>

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ const second = document.getElementById('second');
 
 // This is where the clock functions.
 const clock = setInterval(
-  function time() {
-    const date_now = new Date();;
-    let hr = date_now.getHours();;
-    let min = date_now.getMinutes();;
-    let sec = date_now.getSeconds();;
+  function time () {
+    var date_now = new Date();
+    let hr = date_now.getHours();
+    let min = date_now.getMinutes();
+    let sec = date_now.getSeconds();
 
     // The if statements lets the computer know what to do in. certain situations.
     if (hr < 10) {
-      hr = '0' + hr;;
+      hr = '0' + hr
     }
     if (min < 10) {
-      min = '0' + min;;
+      min = '0' + min
     }
     if (sec < 10) {
-      sec = '0' + sec;;
+      sec = '0' + sec
     }
 
-    hour.textContent = hr;;
-    minute.textContent = min;;
-    second.textContent = sec;;
+    hour.textContent = hr;
+    minute.textContent = min;
+    second.textContent = sec;
   }, 1000
 )
