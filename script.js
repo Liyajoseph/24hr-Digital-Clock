b55c49f7a82f2e42a87f0746b3c4e1b2922a8da1
Liyajoseph <84517633+Liyajoseph@users.noreply.github.com>
2021-06-21 04:17:03 +0000

    // These are the let statements for the program which will let the computer know what stands for what. const hour = document.getElementById('hour'); const minute = document.getElementById('minute'); const second = document.getElementById('second');
    // This is where the clock functions.
    const clock = setInterval(time, 1000);
    function time () {
      const dateNow = new Date();
      let hr = dateNow.getHours();
      let min = dateNow.getMinutes();
      let sec = dateNow.getSeconds();
    
      // The if statements lets the computer know what to do in. certain situations.
      if (hr < 10) {
      hr = '0' + hr
        }
      if (min < 10) {
      min = '0' + min
        }
      if (sec < 10) {
      sec = '0' + sec
        }
    
      hour.textContent = hr;
      minute.textContent = min;
      second.textContent = sec;
      clock.textContent = clock;
    }

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ const second = document.getElementById('second');
 
 // This is where the clock functions.
 const clock = setInterval(time, 1000);
+function time () {
+  const dateNow = new Date();
+  let hr = dateNow.getHours();
+  let min = dateNow.getMinutes();
+  let sec = dateNow.getSeconds();
 
-  function time () {
-    const dateNow = new Date();
-    let hr = dateNow.getHours();
-    let min = dateNow.getMinutes();
-    let sec = dateNow.getSeconds();
-
-    // The if statements lets the computer know what to do in. certain situations.
-    if (hr < 10) {
-      hr = '0' + hr
+  // The if statements lets the computer know what to do in. certain situations.
+  if (hr < 10) {
+  hr = '0' + hr
     }
-    if (min < 10) {
-      min = '0' + min
+  if (min < 10) {
+  min = '0' + min
     }
-    if (sec < 10) {
-      sec = '0' + sec
+  if (sec < 10) {
+  sec = '0' + sec
     }
 
-    hour.textContent = hr;
-    minute.textContent = min;
-    second.textContent = sec;
-  }
+  hour.textContent = hr;
+  minute.textContent = min;
+  second.textContent = sec;
+  clock.textContent = clock;
+}
